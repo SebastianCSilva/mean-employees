15a4640b73a1d4ad59d9ebe084c24103020cac52
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2020-10-14 03:12:20 -0300

    Se arregla el crud y se agrega tanto el obtener por id como el delete por id

--- server/src/controllers/employees.controller.js
+++ server/src/controllers/employees.controller.js
@@ -4,17 +4,28 @@ const employeeCtrl = {}
 const Employee = require('../models/Employee')
 
 employeeCtrl.getEmployees = async (req, res) => {
-    const employees = await Employee.find()
-    res.json(employees)
-}
+    const employees = await Employee.find();
+    res.json(employees);
+};
 
-employeeCtrl.createEmployees = (req, res) => {
-    console.log(req.body)
-    res.send('create employees')
-}
-employeeCtrl.getEmployee = (req, res) => {}
+employeeCtrl.createEmployees = async (req, res) => {
+    const newEmployee = new Employee(req.body);
+    
+    await newEmployee.save();
+
+    res.send({message: "Employee Created"});
+};
+employeeCtrl.getEmployee = async (req, res) => {
+    //console.log(req.params)
+    const employee = await Employee.findById(req.params.id);
+    res.send(employee);
+};
 employeeCtrl.editEmployee = (req, res) => {} 
-employeeCtrl.deleteEmployee = (req, res) => {} 
+
+employeeCtrl.deleteEmployee = async (req, res) => {
+    await Employee.findByIdAndDelete(req.params.id)
+    res.join({status: 'Employee Deleted'})
+}; 
 
 
 module.exports = employeeCtrl
